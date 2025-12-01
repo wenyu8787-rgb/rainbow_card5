76245ac839f7ec9a5e675eafcf99edae5b359f68
wenyu8787-rgb <wenyu8787@gmail.com>
2025-12-01 22:25:57 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+/**
+ * 請注意：將此檔案與應用程式程式碼分開有助於程式碼管理，
+ * 但由於這是客戶端 (Client-side) 配置，API Key 仍會對瀏覽器可見。
+ * 這對於 Firebase Client Key 來說是標準做法。
+ */
+export const firebaseConfig = {
+    apiKey: "AIzaSyB7eOfqSi1NTjDlsvsu5cV3A5wJNAIcLXk",
+    authDomain: "rainbowcard-72101.firebaseapp.com",
+    projectId: "rainbowcard-72101",
+    storageBucket: "rainbowcard-72101.firebasestorage.app",
+    messagingSenderId: "378623295488",
+    appId: "1:378623295488:web:a348cd032073627e3a58e5",
+    measurementId: "G-BLPY7C01XW"
+};
